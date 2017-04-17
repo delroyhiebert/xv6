f7cdb23edddaccc3e23a061fd00544fd832d9199
Delroy <delroy.hiebert@gmail.com>
2017-04-17 00:12:29 -0500

    added new test

--- myMemTest.c
+++ myMemTest.c
@@ -1,62 +1,76 @@
 #include "types.h"
 #include "user.h"
 
-#define PAGE_SIZE 4096
-
-int main(int argc, char *argv[])
+int main (int argc, char* argv[])
 {
-    int* pages[30];
-    int i, pid;
+	if(argc != 2)
+	{
+		printf(1, "Memtest requires one argument.\n");
+		exit();
+	}
 
-    printf(2, "\t starting myMemTest\n");
-    for (i=0; i <= 10; i++) {
-        printf(2, "\t first loop. i=%d\n", i);
-        pages[i] = (int*) malloc(PAGE_SIZE);
-        memset(pages[i], 'z', PAGE_SIZE);
-    }
+	int n = atoi(argv[1]);
+	int i, j;
+	char *heapvar[n];
+	char *pointer;
 
-    for (i = 10; i >=  0; i--) {
-        pages[i][10] = 10;
-        pages[i][100] = 10;
-        pages[i][500] = 10;
-        pages[i][900] = 10;
-        printf(2, "\t second loop. i=%d\n", i);
-    }
+	printf(1, "Interpreting input as: %d\n", n);
+	printf(1, "Attempting to allocate %d page(s) of memory.\n", n);
 
-    if ((pid = fork()) == 0) {
-        printf(2, "\t child: pid=%d\n", getpid());
-        for (i = 10; i >=  0; i--) {
-            pages[i%4][i*i+3] = 66;
-            pages[i][0] = 95;
-            pages[i][20] = 10;
-            pages[i][21] = 'A';
+	//Allocate argv[1] pages
+	printf(1, "Allocating pages: ");
+	for(i = 0; i < n; i++)
+	{
+		heapvar[i] = (char*)malloc(4096); //Allocate a page
+		memset(heapvar[i], (char)i, 4096); //Fill that page with integers.
+		printf(1, "%d ", i);
+	}
+	printf(1, "\n");
 
-            pages[i][120] = 10;
-            pages[i][121] = 'A';
+	//Test fork inheritance
+	if(!fork())
+	{
+		printf(1, "Child reading pages: ");
+		for(i = 0; i < n; i++)
+		{
+			printf(1, "%d ", i);
+			//check each entire page for valid contents
+			for(j = 0; j < 4096; j++)
+			{
+				pointer = heapvar[i];
+				pointer = pointer + j;
+				if (*pointer != (char)i)
+				{
+					printf(1, "Heap has been corrupted. Looking for %d, found %d. Terminating.\n", i, *pointer);
+					exit();
+				}
+			}
+		}
+		printf(1, "\n");
+		exit();
+	}
+	wait();
 
-            pages[i][520] = 10;
-            pages[i][521] = 'A';
+	//Read the content of those pages back in.
+	printf(1, "Parent reading pages: ");
+	for(i = 0; i < n; i++)
+	{
+		printf(1, "%d ", i);
+		//check each entire page for valid contents
+		for(j = 0; j < 4096; j++)
+		{
+			pointer = heapvar[i];
+			pointer = pointer + j;
+			if (*pointer != (char)i)
+			{
+				printf(1, "Heap has been corrupted. Looking for %d, found %d. Terminating.\n", i, *pointer);
+				exit();
+			}
+		}
+	}
+	printf(1, "\n");
 
-            printf(2, "\t third (child) loop. i=%d\n", i);
-        }
-        printf(2, "\t child exiting\n");
-        exit();         /* child exit */
-    } else {
-        pid = fork();
-        if( pid == 0 )
-        {
-            for (i=11; i<17; i++) {
-                printf(2, "forks loop. pid = %d\n", getpid());
-                pages[i] = malloc(PAGE_SIZE);
-                memset(pages[i], 'M', PAGE_SIZE);
-                pages[i%6][1] = 100;
-            }
-        }
-        else {
-            wait();
-        }
-    }
-    wait();
-    printf(2, "\t myMemTest done pid=%d\n", getpid());
-    exit();
+	printf(1, "Page framework test successful.\n");
+
+	exit();
 }
